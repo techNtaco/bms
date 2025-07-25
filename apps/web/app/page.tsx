4a21e6e87e529a8add677c30aa45af7787f64a65
techNtaco <techntaco@gmail.com>
2025-07-26 01:34:56 +0530

    Adding some minor fixes in project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,26 +1,27 @@
 import { client } from "@repo/db/client";
 
-
-export default async function HomePage() {
-  const user = await client.user.findFirst()
-
+export default function HomePage({ user }: { user: any }) {
   if (!user) {
-    return <div>
-      <h1>CI/CD Deployment</h1>
-      <h2>Trigger the CI/CD</h2>
-      <h3>Stranger Things</h3>
-      <p>No user found.</p>
-    </div>
+    return <div>No user found.</div>;
   }
 
   return (
     <main>
       <h1>CI/CD Deployment</h1>
-      <h2>Trigger the CI/CD</h2>
       <h1>Hello World !!!</h1>
       <h1>First User</h1>
       <p><strong>ID:</strong> {user.id}</p>
       <p><strong>Email:</strong> {user.email}</p>
     </main>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const user = await client.user.findFirst();
+
+  return {
+    props: {
+      user: user || null,
+    },
+  };
 }
